16ce9f77579baf17ca7ca45fa0785e85f7715c33
Juned <juned.developer786@gmail.com>
2023-07-19 11:49:08 +0530

    Constroctor Function

--- class20/constructorFunction.js
+++ class20/constructorFunction.js
@@ -1,14 +1,14 @@
-function car(name_of_car,color_of_car,speed_of_car){
+function Car(name_of_car,color_of_car,speed_of_car){
     // console.log(this);
     this.name = name_of_car;
     this.color = color_of_car;
     this.speed = speed_of_car;
 }
 
-let obj1 = new car("Bmw","Black","250km/h");
+let obj1 = new Car("Bmw","Black","250km/h");
 console.log(obj1);
-let obj2 = new car("Ferrari","Red","350km/h");
+let obj2 = new Car("Ferrari","Red","350km/h");
 console.log(obj2);
-let obj3 = new car("Lamborgini","Yellow","400km/h");
+let obj3 = new Car("Lamborgini","Yellow","400km/h");
 console.log(obj3);
 
