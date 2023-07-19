c299c24fa156d94d5e1a83f8976fe445079fd3d2
Juned <juned.developer786@gmail.com>
2023-07-19 11:51:25 +0530

    Constructor Function

---
+++ class21/constructorFxn.js
@@ -0,0 +1,10 @@
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+
+let person1 = new Person("Juned",32);
+console.log(person1);
+
+let person2 = new Person("Akram",29);
+console.log(person2);
