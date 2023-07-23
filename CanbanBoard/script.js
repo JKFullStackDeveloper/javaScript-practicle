ceab28e27934077098338f2cacd3150546d31ac3
Juned <juned.developer786@gmail.com>
2023-07-23 19:53:08 +0530

    delete ticket added

--- CanbanBoard/script.js
+++ CanbanBoard/script.js
@@ -3,51 +3,66 @@ let modal = document.querySelector(".modal-cont");
 let allPriorityColor = document.querySelectorAll(".priority-color");
 let textAreaCont = document.querySelector(".textarea-cont");
 let mainCont = document.querySelector(".main-cont");
+let removeBtn = document.querySelector(".remove-btn");
+
 let addModal = true;
+let removeFlag = false;
 
-addBtn.addEventListener("click",()=>{
-    if(addModal){
-    modal.style.display = "flex";   // show modal
-    addModal=false;                 // setting it false for next click
-    }else{
-     modal.style.display="none";   // on next click hide model
-     addModal=true;                // setting it true for next click
+addBtn.addEventListener("click", () => {
+    if (addModal) {
+        modal.style.display = "flex";   // show modal
+        addModal = false;                 // setting it false for next click
+    } else {
+        modal.style.display = "none";   // on next click hide model
+        addModal = true;                // setting it true for next click
     }
 })
 
-textAreaCont.addEventListener("keydown",(e)=>{
+removeBtn.addEventListener("click", () => {
+    if (removeFlag) {
+        removeBtn.style.color = "black";
+        removeFlag = false
+    } else {
+        removeBtn.style.color = "red";
+        removeFlag = true;
+    }
+})
+
+textAreaCont.addEventListener("keydown", (e) => {
     let key = e.key;
-    if(key=="Enter"){
+    if (key == "Enter") {
         createTicket(e);
-        textAreaCont.value="";
-        modal.style.display="none";
-        addModal=true;
+        textAreaCont.value = "";
+        modal.style.display = "none";
+        addModal = true;
     }
 })
 
-for(let i=0; i<allPriorityColor.length;i++){
-allPriorityColor[i].addEventListener("click",()=>{
-    for(let j=0;j<allPriorityColor.length;j++){
-        if(allPriorityColor[j].classList.contains("active")){
-            allPriorityColor[j].classList.remove("active");
+for (let i = 0; i < allPriorityColor.length; i++) {
+    allPriorityColor[i].addEventListener("click", () => {
+        for (let j = 0; j < allPriorityColor.length; j++) {
+            if (allPriorityColor[j].classList.contains("active")) {
+                allPriorityColor[j].classList.remove("active");
+            }
         }
-    }
-    allPriorityColor[i].classList.add("active");
+        allPriorityColor[i].classList.add("active");
 
-})
+    })
 }
 
-function createTicket(e){
-    
+function createTicket(e) {
+
     let value = e.target.value;
-    if(value == ""){
-        alert("Enter Task Details");
-    }else{
-    let ticketCont = document.createElement("div")
-    ticketCont.setAttribute("class","ticket-cont");
-    ticketCont.innerHTML = `<div class="ticket-color red"></div>
-    <div class="ticket-id">#Eid455</div>
-    <div class="ticket-area">${value}</div>`;
-    mainCont.appendChild(ticketCont);
-    }
+        let ticketCont = document.createElement("div")
+        ticketCont.setAttribute("class", "ticket-cont");
+        ticketCont.innerHTML = `<div class="ticket-color red"></div>
+                                <div class="ticket-id">#Eid455</div>
+                                <div class="ticket-area">${value}</div>`;
+        mainCont.appendChild(ticketCont);
+        ticketCont.addEventListener("click", () => {
+            if (removeFlag) {
+                ticketCont.remove();
+            }
+        })
+    
 }
