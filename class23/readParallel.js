81fd3bd6da48d8790dbfb910c9571924e30a06d6
Juned <juned.developer786@gmail.com>
2023-07-26 14:25:02 +0530

    read file sync and async

--- class23/readParallel.js
+++ class23/readParallel.js
@@ -1,32 +1,32 @@
-// let fs = require("fs");
+let fs = require("fs");
 
 
-// // Asynchronous
+// Asynchronous
 
-// console.log("Before File Reading...");
+console.log("Before File Reading...");
 
-// let file1 = fs.readFile("f1.txt",function cb(error,data){
-//     if(error){
-//         console.log(error)
-//     }else{
-//         console.log("Data of file 1 -> "+ data);
-//     }
-// })
+let file1 = fs.readFile("f1.txt",function cb(error,data){
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Data of file 1 -> "+ data);
+    }
+})
 
-// let file2 = fs.readFile("f2.txt",function cb2(error,data){
-//     if(error){
-//         console.log(error);
-//     }else{
-//         console.log("Data of file 2 -> " + data);
-//     }
-// })
+let file2 = fs.readFile("f2.txt",function cb2(error,data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Data of file 2 -> " + data);
+    }
+})
 
 
-// let file3 = fs.readFile("f3.txt", function cb3(error,data){
-//     if(error){
-//         console.log(error);
-//     }else{
-//         console.log("Data of File 3 -> " + data);
-//     }
-// })
-// console.log("After File Reading...");
+let file3 = fs.readFile("f3.txt", function cb3(error,data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Data of File 3 -> " + data);
+    }
+})
+console.log("After File Reading...");
--- class23/readSerially.js
+++ class23/readSerially.js
@@ -1,16 +1,16 @@
-// let fs = require("fs");
+let fs = require("fs");
 
-// //Syncronous
+//Syncronous
 
-// console.log("Before File read");
+console.log("Before File read");
 
-// let file1 = fs.readFileSync("f1.txt");
-// console.log("Data of file 1 -> " + file1);
+let file1 = fs.readFileSync("f1.txt");
+console.log("Data of file 1 -> " + file1);
 
-// let file2 = fs.readFileSync("f2.txt");
-// console.log("Data of file 1 -> " + file2);
+let file2 = fs.readFileSync("f2.txt");
+console.log("Data of file 1 -> " + file2);
 
-// let file3 = fs.readFileSync("f3.txt");
-// console.log("Data of file 1 -> " + file3);
+let file3 = fs.readFileSync("f3.txt");
+console.log("Data of file 1 -> " + file3);
 
-// console.log("After File read");
+console.log("After File read");
