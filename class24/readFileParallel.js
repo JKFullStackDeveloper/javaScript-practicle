04cce5c1bd0b9ed4791539262c060eef5fd97bd9
Juned <juned.developer786@gmail.com>
2023-07-27 13:52:21 +0530

    Read File Parallel

--- class24/readFileParallel.js
+++ class24/readFileParallel.js
@@ -21,8 +21,15 @@ let file3Promise = fs.promises.readFile("f3.txt");
 function cb(data){
     console.log(""+data);
 }
+
+function errorCb(error){
+    console.log(error);
+}
+
 file1Promise.then(cb);
-
 file2Promise.then(cb);
-
 file3Promise.then(cb);
+
+file1Promise.catch(errorCb);
+file2Promise.catch(errorCb);
+file3Promise.catch(errorCb);
