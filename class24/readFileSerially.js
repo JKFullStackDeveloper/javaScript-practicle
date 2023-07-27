d49c3c5e776aace65c379e6a2c1d35e457db929d
Juned <juned.developer786@gmail.com>
2023-07-27 14:40:13 +0530

    Read File Serially

---
+++ class24/readFileSerially.js
@@ -0,0 +1,42 @@
+let fs = require("fs");
+
+let f1Promise = fs.promises.readFile("f1.txt");
+
+// f1Promise.then(function(data){
+//     console.log(""+data);
+//     let f2Promise = fs.promises.readFile("f2.txt");
+//     return f2Promise;
+// }).then(function(data){
+//     console.log(""+data);
+//     let f3Promise = fs.promises.readFile("f3.txt");
+//     return f3Promise;
+// }).then(function(data){
+//     console.log(""+data);
+// })
+
+// f1Promise.then(function(data){
+//     console.log(""+data)
+//     let f2Promise = fs.promises.readFile("f2.txt")
+//     .then(function(data){
+//         console.log(data+"");
+//         let f3Promise = fs.promises.readFile("f3.txt")
+//         .then(function(data){
+//             console.log(data+"");
+//         })
+//     })
+// })
+
+// Promise Chaining
+
+f1Promise.then(function(data){
+    console.log(""+data);
+    return fs.promises.readFile("f2.txt");
+}).then(function(data){
+    console.log(""+data);
+    return fs.promises.readFile("f3.txt");
+}).then(function(data){
+    console.log(""+data);
+})
+
+
+
