cc2d56685ade3f772869ebfc75cd7c75aec902be
Juned <juned.developer786@gmail.com>
2023-07-28 20:35:57 +0530

    async await added

--- class24/readFileSerially.js
+++ class24/readFileSerially.js
@@ -1,6 +1,9 @@
 let fs = require("fs");
 
 let f1Promise = fs.promises.readFile("f1.txt");
+let f2Promise = fs.promises.readFile("f2.txt");
+let f3Promise = fs.promises.readFile("f3.txt");
+// console.log(f1Promise);
 
 // f1Promise.then(function(data){
 //     console.log(""+data);
@@ -28,15 +31,26 @@ let f1Promise = fs.promises.readFile("f1.txt");
 
 // Promise Chaining
 
-f1Promise.then(function(data){
-    console.log(""+data);
-    return fs.promises.readFile("f2.txt");
-}).then(function(data){
-    console.log(""+data);
-    return fs.promises.readFile("f3.txt");
-}).then(function(data){
-    console.log(""+data);
-})
+// f1Promise.then(function(data){
+//     console.log(""+data);
+//     return fs.promises.readFile("f2.txt");
+// }).then(function(data){
+//     console.log(""+data);
+//     return fs.promises.readFile("f3.txt");
+// }).then(function(data){
+//     console.log(""+data);
+// })
 
+// Async Await
 
+async function readMyFile(){
+    let myfile1 = await f1Promise;
+    console.log(myfile1+"");
+    let myfile2 = await f2Promise;
+    console.log(myfile2+"");
+    let myfile3 = await f3Promise;
+    console.log(myfile3+"");
+}
+
+readMyFile()
 
