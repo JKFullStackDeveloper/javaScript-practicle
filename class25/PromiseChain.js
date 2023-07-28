73eb95515fca4035355caf35835c4aedffa4b3c6
Juned <juned.developer786@gmail.com>
2023-07-28 20:11:06 +0530

    Promise Chaining

--- class25/PromiseChain.js
+++ class25/PromiseChain.js
@@ -1,33 +1,49 @@
-function orderPlaced(drink){
-    
-    return new Promise(function(resolve,reject){
-        if(drink=="Coffee"){
+function orderPlaced(drink) {
+
+    return new Promise(function (resolve, reject) {
+        if (drink == "Coffee") {
             resolve(`Order Placed for ${drink}`);
-        }else{
+        } else {
             reject(`Order not Placed`);
         }
     })
 }
 
-function processOrder(orderStatus){
-    return new Promise(function(resolve){
+function processOrder(orderStatus) {
+    return new Promise(function (resolve) {
         resolve(`${orderStatus} and Processed`);
     })
 }
 
-function generateBill(OrderProcess){
-    return new Promise(function(resolve){
+function generateBill(OrderProcess) {
+    return new Promise(function (resolve) {
         resolve(`${OrderProcess} and Bill is 150 Rs.`);
     })
 }
 
 let orderPromise = orderPlaced("Coffee");
 
-orderPromise.then(function(orderStatus){
+orderPromise.then(function (orderStatus) {
     return processOrder(orderStatus);
-}).then(function(OrderProcess){
+}).then(function (OrderProcess) {
     return generateBill(OrderProcess);
-}).then(function(bill){
+}).then(function (bill) {
     console.log(bill)
+}).catch(function (error) {
+    console.log(error);
 })
+
+
+// orderPromise.then(function (orderStatus) {
+//     return processOrder(orderStatus)
+//         .then(function (OrderProcess) {
+//             return generateBill(OrderProcess);
+//         })
+//         .then(function (bill) {
+//             console.log(bill);
+//         })
+// }).catch(function (error) {
+//     console.log(error);
+// })
+
 
