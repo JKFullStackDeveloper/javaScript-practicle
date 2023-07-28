09eaf2b701ec3399b68006510f628972e21f8413
Juned <juned.developer786@gmail.com>
2023-07-28 19:30:48 +0530

    Promise Chaining

---
+++ class25/PromiseChain.js
@@ -0,0 +1,33 @@
+function orderPlaced(drink){
+    
+    return new Promise(function(resolve,reject){
+        if(drink=="Coffee"){
+            resolve(`Order Placed for ${drink}`);
+        }else{
+            reject(`Order not Placed`);
+        }
+    })
+}
+
+function processOrder(orderStatus){
+    return new Promise(function(resolve){
+        resolve(`${orderStatus} and Processed`);
+    })
+}
+
+function generateBill(OrderProcess){
+    return new Promise(function(resolve){
+        resolve(`${OrderProcess} and Bill is 150 Rs.`);
+    })
+}
+
+let orderPromise = orderPlaced("Coffee");
+
+orderPromise.then(function(orderStatus){
+    return processOrder(orderStatus);
+}).then(function(OrderProcess){
+    return generateBill(OrderProcess);
+}).then(function(bill){
+    console.log(bill)
+})
+
