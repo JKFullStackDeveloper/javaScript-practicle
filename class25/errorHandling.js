c2bc8e46edc8b7b93f48e46aa953fd9dbd2fca2e
Juned <juned.developer786@gmail.com>
2023-07-30 15:50:28 +0530

    error handling

---
+++ class25/errorHandling.js
@@ -0,0 +1,36 @@
+function orderPlaced(drink) {
+    return new Promise(function (resolve, reject) {
+        if (drink == "Coffee") {
+            resolve(`Order Placed for ${drink}`);
+        } else {
+            reject(`Order not Placed`);
+        }
+    })
+}
+
+function processOrder(orderStatus) {
+    return new Promise(function (resolve) {
+        resolve(`${orderStatus} and Processed`);
+    })
+}
+
+function generateBill(OrderProcess) {
+    return new Promise(function (resolve) {
+        resolve(`${OrderProcess} and Bill is 150 Rs.`);
+    })
+}
+
+
+async function cafe(){
+    try{
+        let orderStatus = await orderPlaced("Coffee");
+        let orderProcess = await processOrder(orderStatus);
+        let billGenerate = await generateBill(orderProcess);
+        console.log(billGenerate);
+    }catch(error){
+        console.log(error);
+    }
+    
+}
+
+cafe();
